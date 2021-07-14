1f45f2773949a37d86092149bf2b512a0e7ae4b8
Monish-R45 <81694620+Monish-R45@users.noreply.github.com>
2021-07-14 16:36:14 +0530

    Update Particles.js

--- Particles.js
+++ Particles.js
@@ -2,10 +2,9 @@ class Particle {
     constructor(x, y, r) {
         var options = {
             restitution: 1,
-            friction: 0,
-            isStatic:false
+            friction: 0
         }
-        this.r = 10;
+        this.r = r;
         this.body = Bodies.circle(x, y, this.r, options);
         this.color=color(random(0,255),random(0,255),random(0,255))
         World.add(world, this.body);
